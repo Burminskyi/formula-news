9e483984503b1788b82ada7aeb45a40757f2ff56
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-11-23 00:55:35 +0200

    added dependencises in package.json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/formula">
+      <BrowserRouter basename="/formula-news">
         <App />
       </BrowserRouter>
     </Provider>
